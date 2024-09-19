33bfa596165b677a3cc66f5cf0e698769761b37d
GeovaneVDS <133173278+GeovaneVDS@users.noreply.github.com>
2024-09-19 15:01:43 -0300

    Add files via upload

---
+++ exercicio03/src/Barco.java
@@ -0,0 +1,17 @@
+class Barco {
+    String nome;
+    int tamanho;
+
+    public Barco(String nome, int tamanho) {
+        this.nome = nome;
+        this.tamanho = tamanho;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+}
---
+++ exercicio03/src/PortoGrande.java
@@ -0,0 +1,15 @@
+class PortoGrande extends BasePorto {
+
+    public PortoGrande(String nome) {
+        super(nome);
+    }
+
+    @Override
+    public void atracarBarco(Barco barco) {
+        if (barco.getTamanho() >= 10) {
+            super.atracarBarco(barco);
+        } else {
+            System.out.println(barco.getNome() + " não pode ser atracado no " + nome + " (tamanho menor que 10)");
+        }
+    }
+}
