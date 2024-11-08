7e338acf6bb91d8a3f3a6fd7b28040700ff340c1
GeovaneVDS <133173278+GeovaneVDS@users.noreply.github.com>
2024-11-08 01:47:22 -0300

    Delete exercicio11 directory

--- exercicio11/src/main/java/com/example/exercicio11/dto/TransacaoRequestDTO.java
+++
@@ -1,3 +0,0 @@
-package com.example.exercicio11.dto;
-
-public record TransacaoRequestDTO(String origem, String destino, Double valor) {}
--- exercicio11/src/main/java/com/example/exercicio11/dto/TransacaoResponseDTO.java
+++
@@ -1,3 +0,0 @@
-package com.example.exercicio11.dto;
-
-public record TransacaoResponseDTO(ContaDTO origem, ContaDTO destino, Double valor) {}
